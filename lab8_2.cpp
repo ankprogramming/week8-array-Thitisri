6369b7d335d09f2c018a7440e6a19839a354992a
Unknown <Por.Thitisri@gmail.com>
2018-01-23 12:23:39 +0700

    ทำงานเสร็จแล้วค่ะ23/01/61

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -4,13 +4,28 @@ using namespace std;
 
 int main () {
 
-        char name[] = {"Anukul"};
-        int nameLength = 6;
+//        char city[] = {'A','B','C','\0'};
+//        char city2[] = {"ABC","DEF"}
+
+        char name[] = {"Thitisri"}; // irsitihT
+        int nameLength = 8;
         cout << name;
 
         // TODO: เขียนโปรแกรมเพื่อสลับชื่อ
         // hint : สร้าง array มารับค่าตัวอักษรแบบย้อนกลับ สามารถใช้ loop เข้ามาช่วยได้
 
+        char revName[nameLength+1];
+
+        for (int i= 0; i< nameLength;i++){
+            revName[i] = name [5-i];
+        }
+        revName[nameLength] = '\0';
+        cout << revName <<endl;
+
+        for (int i = 0 ; i <6; i++){
+            revName[i]=revName[i]+1;
+        }
+        cout << revName;
 
 
         return 0;
